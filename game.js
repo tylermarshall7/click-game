cbb158e72618cbe20726097ab679f9b53633c678
tylermarshall7 <tyler.marshall7@gmail.com>
2020-07-05 13:43:52 -0600

    update

--- game.js
+++ game.js
@@ -1,1 +1,33 @@
-!!
+let fossil = 0
+
+
+
+// clicker upgrades // 
+
+let clickUpgrades = {
+  shovels: {
+    price: 100,
+    quantity: 0,
+    multiplier: 1
+  }
+}
+
+
+// Automatic Upgrades //
+let autoUpgrades = {
+  Resetti: {
+    price: 200,
+    quantity: 0,
+    multiplier: 10
+  }
+}
+
+
+// methods //
+
+
+function dig() {
+  fossil++
+}
+
+
